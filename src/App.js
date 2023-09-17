853c9de06cc94dbe43b67ca71f6e049f2da4dd7f
Phan Kien <phankien110302@gmail.com>
2023-09-17 14:38:56 +0700

    modify librari

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import WebRouter from './router';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 function App() {
-  library.add(fas)
+  library.add(fas);
   return (
     <div className="App">
-          <WebRouter/>
+          <Provider store={store}>
+              <WebRouter/>
+          </Provider>
     </div>
   );
 }
