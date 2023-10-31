5d0a7458b42ed355bf6aeb86546f02327773096f
Phan Kien <phankien110302@gmail.com>
2023-11-01 01:07:26 +0700

    complete acitve and reset key

--- src/page/authentication/activeAccoutPage/activeAccout.js
+++ src/page/authentication/activeAccoutPage/activeAccout.js
@@ -1,17 +1,39 @@
 import { Button, Form, Input } from "antd";
-import styles from "./style.module.scss"
-import { useSelector,useDispatch } from "react-redux";
-import request from "../../../utils/httpRequest"
-import { useEffect, useState } from "react";
+import styles from "./style.module.scss";
+import { useSelector } from "react-redux";
+import request from "../../../utils/httpRequest";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 function ActivePage() {
-    const login = useSelector(state =>state.authentication)
-    // const [key , setkey] = useState(null);
-    const resetKey = ()=>{
-
-    }
-    const setkey = (event) =>{
-        console.log(event.target.value);
-    }
+    const navigate = useNavigate();
+    const [inputkey, setInputkey] = useState();
+    const [message,setMessage] = useState(" ");
+    const login = useSelector((state) => state.authentication);
+    const activeAccout = async () => {
+        await request.post("/active-user",{
+            active_key : inputkey
+        }).then(()=>{
+            navigate("/")
+        }).catch((error)=>{
+            console.log(error.response.data);
+            let message = error.response.data.message;
+            console.log(message);
+            setMessage(message)
+        });
+    };
+    const resetKey = async () => {
+        await request.post("reset-active-key",{
+            email:login.user.email
+        }).then((response)=>{
+            console.log(response);
+            setMessage(response.data.message)
+        }).catch((error)=>{
+            setMessage(error.response.data.message)
+        })
+    };
+    const setKey = (event) => {
+        setInputkey(event.target.value);
+    };
     return (
         <div className={styles.activePage}>
             <h1 className={styles.title}>Active Accout</h1>
@@ -45,26 +67,24 @@ function ActivePage() {
                             required: true,
                             message: "Please input your active key!",
                         },
-                    ]}
-                    getValueFromEvent={setkey}
-                    >
-                    <Input placeholder="active key" />
+                    ]}>
+                    <Input placeholder="active key" onChange={setKey} />
                 </Form.Item>
-                <Form.Item
-                        className={`${styles.action}`}>
-                        <Button
-                            className={styles.button}
-                            type="warning"
-                            htmlType="button">
-                            Reset key
-                        </Button>
-                        <Button
-                            className={`${styles.button}`}
-                            type="success"
-                            htmlType="button">
-                            Active
-                        </Button>
-                    </Form.Item>
+                <p className={`text-danger ${styles.message}`} >{message}</p>
+                <Form.Item className={`${styles.action}`}>
+                    <Button
+                        className={styles.button}
+                        onClick={resetKey}
+                        htmlType="button">
+                        Reset key
+                    </Button>
+                    <Button
+                        className={`${styles.button}`}
+                        htmlType="button"
+                        onClick={activeAccout}>
+                        Active
+                    </Button>
+                </Form.Item>
             </Form>
         </div>
     );
